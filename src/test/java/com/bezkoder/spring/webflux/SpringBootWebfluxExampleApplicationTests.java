1f2284e8aad572115b0cc421e2c37adb6c3447f9
tienbku <tien.ngtruong@gmail.com>
2023-02-11 10:03:49 +0700

    first commit

---
+++ src/test/java/com/bezkoder/spring/webflux/SpringBootWebfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezkoder.spring.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
